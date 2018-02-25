3e1d4fe3ef8c99240c8484f37d9fd2369602db9d
Jacob Gagne <jacob_gagne@hotmail.com>
2018-02-24 21:59:20 -0500

    more tests

--- modules/game/src/test/GameTest.scala
+++ modules/game/src/test/GameTest.scala
@@ -1,9 +1,9 @@
 package lila.game
 
 import chess.Color.{ Black, White }
-import chess.{ Status, UnmovedRooks }
+import chess.{ Color, Status, UnmovedRooks }
 import lila.db.ByteArray
-import lila.game.Source.Lobby
+import lila.game.Source.{ Import, Lobby }
 import org.mockito.Mockito._
 import org.specs2.mutable._
 
@@ -17,50 +17,32 @@ class GameTest extends Specification {
   private val defaultMockMetadata = mock(classOf[Metadata])
 
   // ByteArray instances (parameter for new Game)
-  // NOTE: Could not mock ByteArray, received error saying unable to mock static/final classes. Will look into it
-  val byteArray1 = new ByteArray(Array())
-  val byteArray2 = new ByteArray(Array())
+  val byteArray1 = ByteArray(Array())
+  val byteArray2 = ByteArray(Array())
 
-  // Mock UnmovedRooks class (parameter for new Game)
+  // UnmovedRooks class (parameter for new Game)
   private val mockUnmovedRooks = UnmovedRooks(Set())
 
-  // Color values
-  private val white = White
-  private val black = Black
+  when(mockWhitePlayer.color).thenReturn(White)
+  when(mockBlackPlayer.color).thenReturn(Black)
 
-  when(mockWhitePlayer.color).thenReturn(white)
-  when(mockBlackPlayer.color).thenReturn(black)
-
-  // Other
   var gameId = "22222"
 
-  def getGameInstance(metadata: Metadata, whitePlayer: Player = mockWhitePlayer): Game =
-    Game(id = gameId, whitePlayer = mockWhitePlayer, blackPlayer = mockBlackPlayer, binaryPgn = byteArray1,
-      binaryPieces = byteArray2, turns = 0, startedAtTurn = 0, status = Status.Created, castleLastMoveTime = null,
+  def getGameInstance(metadata: Metadata = defaultMockMetadata, whitePlayer: Player = mockWhitePlayer,
+    blackPlayer: Player = mockBlackPlayer, status: Status = Status.Created): Game =
+    Game(id = gameId, whitePlayer = whitePlayer, blackPlayer = blackPlayer, binaryPgn = byteArray1,
+      binaryPieces = byteArray2, turns = 0, startedAtTurn = 0, status = status, castleLastMoveTime = null,
       unmovedRooks = mockUnmovedRooks, metadata = metadata, daysPerTurn = Some(1))
 
-  //-----------------
-  //     TESTS
-  //-----------------
-
-  // player(color: Color)
   "Players" should {
+
     "have a defining color" in {
-      val game = getGameInstance(defaultMockMetadata)
+      val game = getGameInstance()
 
-      game.player(white) must be(mockWhitePlayer)
-      game.player(black) must be(mockBlackPlayer)
-    }
-  }
-
-  // opponent(p: Player), opponent(c: Color)
-  "Opponent" should {
-    "have opposite color" in {
-      val mockPlayer = mock(classOf[Player])
-      val game = getGameInstance(defaultMockMetadata, mockPlayer)
-
-      when(mockPlayer.color).thenReturn(white)
-      game.opponent(mockPlayer) must be(mockBlackPlayer)
+      mockWhitePlayer.color must be(White)
+      mockBlackPlayer.color must be(Black)
+      game.player(White) must be(mockWhitePlayer)
+      game.player(Black) must be(mockBlackPlayer)
     }
   }
 
@@ -70,6 +52,8 @@ class GameTest extends Specification {
       val mockMetadata = mock(classOf[Metadata])
       val game = getGameInstance(mockMetadata)
 
+      when(mockMetadata.tournamentId).thenReturn(None)
+      game.isTournament must beFalse
       when(mockMetadata.tournamentId).thenReturn(Some("123"))
       game.isTournament must beTrue
       game.hasChat must beFalse
@@ -80,6 +64,9 @@ class GameTest extends Specification {
       val game = getGameInstance(mockMetadata)
 
       when(mockMetadata.tournamentId).thenReturn(None)
+      when(mockMetadata.simulId).thenReturn(None)
+      game.isSimul must beFalse
+      when(mockMetadata.tournamentId).thenReturn(None)
       when(mockMetadata.simulId).thenReturn(Some("456"))
       game.isSimul must beTrue
       game.hasChat must beFalse
@@ -89,6 +76,9 @@ class GameTest extends Specification {
       val mockMetadata = mock(classOf[Metadata])
       val game = getGameInstance(mockMetadata)
 
+      when(mockMetadata.tournamentId).thenReturn(None)
+      when(mockMetadata.simulId).thenReturn(None)
+      game.isMandatory must beFalse
       when(mockMetadata.tournamentId).thenReturn(Some("123"))
       when(mockMetadata.simulId).thenReturn(Some("456"))
       game.isMandatory must beTrue
@@ -109,18 +99,70 @@ class GameTest extends Specification {
       game.hasChat must beTrue
     }
 
-    "should have white as start color" in {
-      val game = getGameInstance(defaultMockMetadata)
+    "have white as start color" in {
+      val game = getGameInstance()
       mockWhitePlayer.color must be(chess.Color.White)
       game.startColor must be(chess.Color.White)
     }
 
-    "should be playable by default" in {
+    "be playable by default" in {
       val lobbyMetadata = mock(classOf[Metadata])
       when(lobbyMetadata.source).thenReturn(Some(Lobby))
 
       val game = getGameInstance(lobbyMetadata)
+      game.fromLobby must beTrue
       game.playable must beTrue
+    }
+
+    "not be playable if imported or aborted" in {
+      val importedMetadata = mock(classOf[Metadata])
+      when(importedMetadata.source).thenReturn(Some(Import))
+
+      val importedGame = getGameInstance(importedMetadata)
+
+      importedGame.imported must beTrue
+      importedGame.playable must beFalse
+      getGameInstance(status = Status.Aborted).playable must beFalse
+    }
+
+    "return correct player and opponent values" in {
+      val game = getGameInstance()
+      game.player must be(mockWhitePlayer)
+      game.opponent(game.player) must be(mockBlackPlayer)
+      game.opponent(mockWhitePlayer) must be(mockBlackPlayer)
+      game.opponent(mockBlackPlayer) must be(mockWhitePlayer)
+    }
+
+    "return the right amount of turns played" in {
+      val game = spy(getGameInstance())
+      when(game.startedAtTurn).thenReturn(0)
+      when(game.turns).thenReturn(10)
+      game.playedTurns must beEqualTo(10)
+
+      when(game.startedAtTurn).thenReturn(5)
+      game.playedTurns must beEqualTo(5)
+    }
+
+    "flag games that are out of time" in {
+      val game = spy(getGameInstance())
+      game.flagged must be(None)
+
+      when(game.status).thenReturn(Status.Outoftime)
+      game.flagged must beSome[Color](White)
+    }
+
+    "consider calculate proper average rating" in {
+      val weakPlayer = mock(classOf[Player])
+      val strongPlayer = mock(classOf[Player])
+
+      when(weakPlayer.rating).thenReturn(Some(1000))
+      when(strongPlayer.rating).thenReturn(Some(2000))
+
+      val game = spy(getGameInstance(whitePlayer = weakPlayer, blackPlayer = strongPlayer))
+      game.whitePlayer must be(weakPlayer)
+      game.blackPlayer must be(strongPlayer)
+      game.players must beEqualTo(List(weakPlayer, strongPlayer))
+      game.averageUsersRating must beSome[Int](1500)
     }
   }
 }
