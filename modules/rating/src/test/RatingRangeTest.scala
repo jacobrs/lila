c4d6ad8c0791a14f9c637aa750ea0caa62821769
benrs <benjaminbarault@gmail.com>
2018-02-25 14:31:43 -0500

    Removed new keyword and used beEqualTo instead

--- modules/rating/src/test/RatingRangeTest.scala
+++ modules/rating/src/test/RatingRangeTest.scala
@@ -4,34 +4,34 @@ import org.specs2.mutable._
 class RatingRangeTest extends Specification {
   "RatingRange" should {
     "set a proper default" in {
-      val rr = new RatingRange(1000, 2000);
+      val rr = RatingRange(1000, 2000);
 
-      rr.toString mustEqual ("1000-2000")
+      rr.toString must beEqualTo("1000-2000")
     }
 
     "properly contain a value within its range" in {
-      val rr = new RatingRange(1499, 1501);
+      val rr = RatingRange(1499, 1501);
       rr.contains(1500) must beTrue
     }
 
     "properly exclude a values out of its range" in {
-      val rr = new RatingRange(1499, 1501);
+      val rr = RatingRange(1499, 1501);
       rr.contains(1498) must beFalse
       rr.contains(1502) must beFalse
     }
 
     "ignore the min if it passes the RatingRange min" in {
-      val rr = new RatingRange(200, 1501);
+      val rr = RatingRange(200, 1501);
       rr.contains(100) must beTrue
     }
 
     "ignore the max if it passes the RatingRange max" in {
-      val rr = new RatingRange(1499, 3000);
+      val rr = RatingRange(1499, 3000);
       rr.contains(1000000) must beTrue
     }
 
     "ignore the min and max if they pass their respective RatingRange values" in {
-      val rr = new RatingRange(100, 3000);
+      val rr = RatingRange(100, 3000);
       rr.contains(-1000000) must beTrue
       rr.contains(1000000) must beTrue
     }
