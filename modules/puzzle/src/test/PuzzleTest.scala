d0646a5e37a4d6e6f8ce6a57364eb7b6e8a702f1
bdschoeler <bdschoeler@gmail.com>
2018-04-06 12:48:07 -0400

    Removed commented out code, and refactored code

--- modules/puzzle/src/test/PuzzleTest.scala
+++ modules/puzzle/src/test/PuzzleTest.scala
@@ -29,16 +29,6 @@ class PuzzleTest extends Specification {
     val testBool = false
     var puzzleListBuffer = ListBuffer[Puzzle]()
 
-    //    val puzzle1 = Puzzle(1, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle2 = Puzzle(2, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle3 = Puzzle(3, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle4 = Puzzle(4, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle5 = Puzzle(5, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle6 = Puzzle(6, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle7 = Puzzle(7, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle8 = Puzzle(8, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-    //    val puzzle9 = Puzzle(9, testString, null, testString, null, 1, null, null, null, null, 1, testBool)
-
     lazy val api = new PuzzleApi(
       puzzleColl = db.puzzleColl,
       puzzleMigrationColl = db.puzzleMigrationColl,
@@ -55,8 +45,8 @@ class PuzzleTest extends Specification {
 
     val puzzleList = puzzleListBuffer.toList
 
-    for (i <- 0 to 8)
-      api.puzzle.insertPuzzleToOld(puzzleList(i))
+    for (puzzle <- puzzleList)
+      api.puzzle.insertPuzzleToOld(puzzle)
 
     "the api" should {
       "not be null" in {
